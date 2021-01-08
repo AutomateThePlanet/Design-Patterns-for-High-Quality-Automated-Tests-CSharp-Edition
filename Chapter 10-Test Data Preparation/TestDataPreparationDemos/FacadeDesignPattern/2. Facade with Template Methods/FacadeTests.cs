a90c2392b8c2752f7f800dcee8a397e21be92435
Anton Angelov <aangelov@automatetheplanet.com>
2021-01-08 18:43:08 +0200

    update chapter 10
    upgrade chapter 10 to support .net 5
    update all nuget packages
    add editor config
    add license headers

--- Chapter 10-Test Data Preparation/TestDataPreparationDemos/FacadeDesignPattern/2. Facade with Template Methods/FacadeTests.cs
+++ Chapter 10-Test Data Preparation/TestDataPreparationDemos/FacadeDesignPattern/2. Facade with Template Methods/FacadeTests.cs
@@ -1,3 +1,13 @@
+﻿// Copyright 2021 Automate The Planet Ltd.
+// Author: Anton Angelov
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 using AutoFixture;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -38,7 +48,7 @@ namespace TestDataPreparationDemos.Facades.Second
             var fixture = new Fixture();
             purchaseInfo.Note = fixture.Create<string>();
 
-            _purchaseFirstVersionFacade.PurchaseItem("Saturn V", "happybirthday", 3, "355.00�", purchaseInfo);
+            _purchaseFirstVersionFacade.PurchaseItem("Saturn V", "happybirthday", 3, "355.00€", purchaseInfo);
         }
 
         // .NET Core does not support the DataSource attribute. If you try to access test data in this way in a .NET Core or UWP unit test project, 
